548dea5d2238d3ba324f59a0df0ca1c4767e9d41
Erebe11 <corentin.soulier@gmail.com>
2017-05-29 21:12:57 +0200

    jeton tostring

--- LaserMines/Jeton.cs
+++ LaserMines/Jeton.cs
@@ -11,5 +11,31 @@ namespace LaserMines
         public Type type = Type.joueur1;
 
         public Jeton(Type t) { type = t; }
+
+        override public String ToString()
+        {
+            String res = "";
+
+            switch (type)
+            {
+                case Type.bloc:
+                    res += "#";
+                    break;
+                case Type.joueur1:
+                    res += "X";
+                    break;
+                case Type.joueur2:
+                    res += "O";
+                    break;
+                case Type.neutre:
+                    res += "*";
+                    break;
+                default:
+                    res += " ";
+                    break;
+            }
+
+            return res;
+        }
     }
 }
